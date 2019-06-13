b156f620a48c37fa54cb58960b0fcf7f8b950e8b
Magdalena Stopka <m.stopka@microsoft.wsei.edu.pl>
2019-06-13 14:46:45 +0200

    queuingReflection

---
+++ QueuingLibrary/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueuingLibrary
+{
+    public class Main
+    {
+       public static void Run(string codeAction, string objectId )
+        {
+            Console.WriteLine("Odnaleziono dll");
+        }
+    }
+}
---
+++ TescikQueuing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TescikQueuing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFileFromLibs();
+            Console.ReadKey();
+        }
+        public static void GetFileFromLibs()
+        {
+             var files = Directory.GetFiles(@"C:\Users\Admin\Desktop\QueuingLibrary\TescikQueuing\Libs", "*.dll");
+            foreach (var file in files)
+            {
+                Type[] types;
+                ////https://www.codeproject.com/Articles/19911/Dynamically-Invoke-A-Method-Given-Strings-with-Met
+                ////https://stackoverflow.com/questions/20240092/use-reflection-to-search-in-dlls-c-sharp
+
+                types = Assembly.LoadFrom(file).GetTypes();
+
+                foreach (var type in types)
+                {
+                    MethodInfo m = type.GetMethod("Run", new Type[] { typeof(string), typeof(string) });
+  
+                    if (m != null)
+
+                        Console.WriteLine("Found method: {0}", m);
+                    m.Invoke(null, new object[] {null, null });
+                    //MethodInfo[] methods = type.GetMethods();
+                    //foreach(var method in methods)
+                    //{ Console.WriteLine(method); }
+                }
+
+            }
+
+        }
+    }
+}
+
